d24583bc513b3c796826634bcddae1c4e96cfa15
Andreas <hamburger1984@gmail.com>
2021-07-31 20:59:09 +0200

    Push data to ThingSpeak

--- ElectricRain/src/main.cpp
+++ ElectricRain/src/main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
+
 #include "WiFi.h"
-#include "WifiCredentials.h"
+#include "WifiSecrets.h"
+
+#include "ThingSpeak.h"
+#include "ThingSpeakSecrets.h"
 
 #define SENSOR1 33
 #define SENSOR2 32
@@ -14,30 +18,40 @@
 
 #define WIFI_TIMEOUT_MS 20000
 
-void connectToWifi(){
+WiFiClient wifiClient;
+
+void connectToWifi()
+{
   Serial.printf("Connecting to WiFi '%s'", WIFI_NETWORK);
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
 
   unsigned long startWifi = millis();
 
-  while(WiFi.status() != WL_CONNECTED && millis() - startWifi < WIFI_TIMEOUT_MS){
+  while (WiFi.status() != WL_CONNECTED && millis() - startWifi < WIFI_TIMEOUT_MS)
+  {
     Serial.print(".");
     delay(250);
   }
 
-  if(WiFi.status() != WL_CONNECTED){
+  if (WiFi.status() != WL_CONNECTED)
+  {
     Serial.println(" ! failed !");
-    // deep sleep?!
-  } else {
+    sleep(60);
+  }
+  else
+  {
     Serial.print(" connected ");
     Serial.println(WiFi.localIP());
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
+
   connectToWifi();
+  ThingSpeak.begin(wifiClient);
 
   pinMode(SENSOR1, INPUT);
   pinMode(SENSOR2, INPUT);
@@ -55,25 +69,69 @@ void setup() {
   digitalWrite(PUMP4, HIGH);
 }
 
-
-void processSensor(uint8_t logical, uint8_t readPin, uint8_t writePin){
+int processSensor(uint8_t field, uint8_t readPin, uint8_t writePin)
+{
   float value = analogRead(readPin);
 
-  Serial.printf("%d (%d) = %f ", logical, readPin, value);
+  Serial.printf("%d (%d) = %f ", field, readPin, value);
+  ThingSpeak.setField(field, value);
 
-  if(value > 3000){
+  if (value > 3000)
+  {
     digitalWrite(writePin, HIGH);
-  } else {
+    return 0;
+  }
+  else
+  {
     digitalWrite(writePin, LOW);
+    return 1;
   }
 }
 
-void loop() {
-  processSensor(1, SENSOR1, PUMP1);
-  processSensor(2, SENSOR2, PUMP2);
-  processSensor(3, SENSOR3, PUMP3);
-  processSensor(4, SENSOR4, PUMP4);
+void loop()
+{
+  String status = "";
 
-  Serial.println();
-  delay(750);
+  if (processSensor(1, SENSOR1, PUMP1))
+  {
+    status += "PUMPING 1";
+  }
+  if (processSensor(2, SENSOR2, PUMP2))
+  {
+    if (status.length() > 0)
+    {
+      status += ", ";
+    }
+    status += "PUMPING 2";
+  }
+  if (processSensor(3, SENSOR3, PUMP3))
+  {
+    if (status.length() > 0)
+    {
+      status += ", ";
+    }
+    status += "PUMPING 3";
+  }
+  if (processSensor(4, SENSOR4, PUMP4))
+  {
+    if (status.length() > 0)
+    {
+      status += ", ";
+    }
+    status += "PUMPING 4";
+  }
+
+  if (status.length() == 0)
+  {
+    status = "-- OFF --";
+  }
+
+  Serial.println(status);
+  ThingSpeak.setStatus(status);
+
+  ThingSpeak.setField(8, WiFi.RSSI());
+
+  ThingSpeak.writeFields(TP_CHANNEL, TP_WRITE_API_KEY);
+
+  delay(10000);
 }
